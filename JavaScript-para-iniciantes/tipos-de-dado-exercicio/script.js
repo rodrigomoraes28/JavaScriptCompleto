35bc27ff6d49ec1ed6a97b7e8171101a3cb927c2
Rodrigo Moraes <rodrigostmoraes@gmail.com>
2023-03-07 22:57:48 -0300

    exercicio
    exercicio

---
+++ JavaScript-para-iniciantes/tipos-de-dado-exercicio/script.js
@@ -0,0 +1,38 @@
+// Declare uma variável contendo uma string
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
+
+
+var sentenca = "Sou de maior";
+console.log(sentenca);
+
+// var frase = "Sou de maior e tenho " + 18 + "  anos";
+// console.log(frase);
+var ano = '2023';
+console.log(ano);
+
+var idade = 18;
+
+var nome = "Rodrigo";
+var sobrenome = "Moraes";
+
+console.log(nome + sobrenome);
+
+var nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+
+
+var nome = nome + " It's time";
+console.log(nome);
+
+console.log(typeof nome);
+
